4ad47273d090f78298226faee37f7f8bc59ed3ec
Roman Bartusiak <roman@stashaway.com>
2022-01-17 18:50:13 +0100

    feat: t2

---
+++ lib/src/main/kotlin/ml/bartusiak/euler/T2.kt
@@ -0,0 +1,22 @@
+package ml.bartusiak.euler
+
+class T2 {
+
+    companion object{
+        @JvmStatic
+        fun main(vararg args: String){
+            println(T2().solve())
+        }
+    }
+
+
+
+    fun solve(): Long {
+        return generateSequence(Pair(1L,2L)){
+            Pair(it.second, it.first+it.second)
+        }.map { it.first }.takeWhile { it<4_000_000 }.filter { it%2==0L }.sum()
+    }
+
+
+}
+
