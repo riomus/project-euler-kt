60ede3807505888f67b5ba75fe5b2d665a60ba64
Roman Bartusiak <roman@stashaway.com>
2022-03-31 10:15:39 +0200

    feat: T8

---
+++ lib/src/main/kotlin/ml/bartusiak/euler/T8.kt
@@ -0,0 +1,22 @@
+package ml.bartusiak.euler
+
+import kotlin.math.ceil
+import kotlin.math.sqrt
+
+class T8 {
+
+    companion object{
+        @JvmStatic
+        fun main(vararg args: String){
+            println(T8().solve())
+        }
+    }
+    fun Long.square(): Long = this * this
+
+    fun solve(): Long {
+        return T8::class.java.getResource("/data/T8.txt").readText().split("", "\n").filter{it.isNotEmpty()}.filter{it != "\n"}.map { it.toLong() }.windowed(13).maxOf { it.reduce{a, b -> a*b} }
+    }
+
+
+}
+
